ffb56eb5e7e6262cbd633f9f9616222895ec0ea4
FlorinIO <florin.iordache@jordachewd.ro>
2024-11-19 16:47:45 +0200

    more style fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,10 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
+import Head from "next/head";
 import theme from "@/theme";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./globals.css";
-import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -23,10 +23,10 @@ export default function RootLayout({
         <Head>
           <meta
             name="viewport"
-            content="width=device-width, user-scalable=no"
+            content="width=device-width, initial-scale=1.0, user-scalable=no"
           />
         </Head>
-        <body className="antialiased">
+        <body>
           <AppRouterCacheProvider>
             <ThemeProvider theme={theme}>
               <CssBaseline />
