1427fd9916c7ac4f9086f5ba7ff69e43764c5843
JWD <jordachewd@users.noreply.github.com>
2024-11-23 11:03:39 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   width: "device-width",
+  height: "device-height",
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
