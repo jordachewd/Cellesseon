3cb6aa4c44d0f8895d25ed2cdaed7f304e062bf6
FlorinIO <florin.iordache@jordachewd.ro>
2025-01-14 18:45:57 +0200

    clerkMiddleware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -6,6 +6,7 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api/webhooks/stripe",
+  "/api/webhooks/clerk",
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
