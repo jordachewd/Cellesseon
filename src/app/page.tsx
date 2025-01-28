6e013c1115dd29ead25946256f16cf264d3cd635
FlorinIO <florin.iordache@jordachewd.ro>
2025-01-28 17:40:05 +0200

    signOut button in case of fail pulling userData

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs/server";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { getUserById } from "@/lib/actions/user.actions";
 import { UserData } from "@/types/UserData.d";
+import { SignOutButton } from "@clerk/nextjs";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import LandingPage from "@/components/sections/LandingPage";
@@ -11,14 +12,13 @@ import AlertMessage from "@/components/shared/AlertMessage";
 
 export default async function Home() {
   const { userId } = await auth();
+
   let userData: UserData | undefined = undefined;
 
   if (userId) {
     const getUserData = await getUserById(userId);
     userData = getUserData.user;
   }
-
-  // console.log("userData:", userData);
 
   return (
     <>
@@ -29,17 +29,20 @@ export default async function Home() {
       </SignedOut>
 
       <SignedIn>
-        {!userData && (
-          <AlertMessage
-            message={{
-              title: "Error getting user data!",
-              text: `User does not exist!`,
-            }}
-          />
-        )}
-
         {userId && userData ? (
           <ChatWrapper userData={userData} />
+        ) : !userData ? (
+          <>
+            <AlertMessage
+              message={{
+                title: "Error getting user data!",
+                text: `User does not exist!`,
+              }}
+            />
+            <div className="flex justify-center items-center h-dvh z-10 text-2xl" >
+              <SignOutButton />
+            </div>
+          </>
         ) : (
           <LoadingBubbles wrapped />
         )}
